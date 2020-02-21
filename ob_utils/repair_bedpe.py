87f5c0c6eeb2767bcd62fe985c07d8cc696ee53e
ken0-1n <ubuntu@ip-172-31-34-116.ap-northeast-1.compute.internal>
2020-02-21 01:53:55 +0000

    add gridss

--- ob_utils/repair_bedpe.py
+++ ob_utils/repair_bedpe.py
@@ -26,6 +26,38 @@ def repair_dup_strand(bedpe_file, output):
     hOUT.close()     
     
 
+def repair_sv_type(bedpe_file, output):
+    
+    hOUT = open(output, 'w')
+    with open(bedpe_file, 'r') as hin:
+        for line in hin:
+            
+            if line.startswith("#"):
+                header = line.rstrip('\n')
+                print(header, file=hOUT)
+                continue
+            line = line.rstrip('\n')
+            F = line.split('\t')
+
+            chromA = F[0]
+            chromB = F[3]
+            strandA = F[8]
+            strandB = F[9]
+            sv_type = F[10]
+           
+            if chromA == chromB and strandA == '-' and strandB == '+':
+                sv_type = "DUP"
+            elif chromA == chromB and strandA == '+' and strandB == '-':
+                sv_type = "DEL"
+            elif chromA == chromB and strandA == '+' and strandB == '+':
+                sv_type = "INV"
+            elif chromA == chromB and strandA == '-' and strandB == '-':
+                sv_type = "INV"
+
+            print('\t'.join(F[0:10])+'\t'+sv_type+'\t'+ '\t'.join(F[11:]), file=hOUT)
+    hOUT.close()    
+
+
 def repair_bedpe_main(args):
 
     repair_dup_strand(args.in_bedpe, args.output)
